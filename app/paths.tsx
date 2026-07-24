4de56f062a993dd237bab22282eb66ca15cf6d52
Joseph Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2026-07-24 23:38:46 +0300

    Refactor routing and path configuration

---
+++ app/paths.tsx
@@ -0,0 +1,6 @@
+export const sidebarPath = () => "/sidebar";
+export const aboutPath = () => "/about";
+export const portfolioPath = () => "/portfolio";
+export const contactPath = () => "/contact";
+export const homePath = () => "/";
+export const projectsPath = () => "/projects";
