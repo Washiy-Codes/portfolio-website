c393559f494bb4a797beaabb41c3283116865620
Joseph Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2026-07-24 23:37:39 +0300

    Add shared data file for dynamic content

---
+++ app/data.tsx
@@ -0,0 +1,109 @@
+ const projects = [
+  {
+    "title": "Survey Form",
+    "image": "https://global.discourse-cdn.com/freecodecamp/optimized/4X/3/2/d/32d3850d9916f873690eda1d2ec929af416fc87d_2_690x317.jpeg",
+    "description": "A responsive survey form built with HTML and CSS to collect and organize user feedback",
+    "github": "#",
+    "liveDemo": "#"
+  },
+  {
+    "title": "Tribute Page",
+    "image": "https://images.theconversation.com/files/610544/original/file-20240731-17-1rkmgd.jpg?ixlib=rb-4.1.0&rect=3%2C0%2C1019%2C797&q=45&auto=format&w=926&fit=clip",
+    "description": "A tribute page dedicated to a person or event, showcasing their significance.",
+    "github": "#",
+    "liveDemo": "http://127.0.0.1:3002/tp.html"
+  },
+  {
+    "title": "Landing Page",
+    "image": "https://user-images.githubusercontent.com/34116562/41981994-ef9be29a-7a47-11e8-8806-f5dc0ecca7f3.png",
+    "description": "A responsive landing page built with HTML and CSS to showcase a product or service.",
+    "github": "https://github.com/Washiy-Codes/Product-Landing-Page",
+    "liveDemo": "https://washiy-codes.github.io/Product-Landing-Page/"
+  },
+  {
+    "title": "A Cash Register App",
+    "image": "https://global.discourse-cdn.com/freecodecamp/original/4X/0/5/2/0527b12f7f064476768b3de9605b6c869d8d26b2.png",
+    "description": "A web app that calculates change using available coins/notes.",
+    "github": "https://github.com/Washiy-Codes/Cash-Register-App",
+    "liveDemo": "https://washiy-codes.github.io/Cash-Register-App/"
+  },
+  {
+    "title": "RPG Creature Search App",
+    "image": "https://opengraph.githubassets.com/124739121f8d6a88420a5598c386a5dd6a196e91db61d14b86a1bbee36fd5586/freeCodeCamp/freeCodeCamp/issues/59048",
+    "description": "Fetches and displays RPG creatures with stats from an API.",
+    "github": "https://github.com/Washiy-Codes/RPG-Search-App",
+    "liveDemo": "https://washiy-codes.github.io/RPG-Search-App/"
+  },
+  {
+    "title": "Weather App",
+    "image": "https://cdn-media-1.freecodecamp.org/images/0*e-_dbhFTqw7WMHwg.png",
+    "description": "A weather application that provides real-time and forecasted weather data through API integration.",
+    "github": "https://github.com/Washiy-Codes/Weather-App",
+    "liveDemo": "https://washiy-codes.github.io/Weather-App/"
+  },
+  {
+    "title": "Random Quote Machine",
+    "image": "https://cdn.freecodecamp.org/testable-projects-fcc/images/random-quote-machine.png",
+    "description": "A simple app that generates and displays random inspirational quotes with a single click.",
+    "github": "https://github.com/Washiy-Codes/Quote-Machine",
+    "liveDemo": "https://washiy-codes.github.io/Quote-Machine/"
+  },
+  {
+    "title": "Palindrome Checker",
+    "image": "https://global.discourse-cdn.com/freecodecamp/optimized/4X/b/d/3/bd3b145a689f12b5c71d40668a6df6cb9038b422_2_690x319.png",
+    "description": "A simple tool that checks whether a word or phrase reads the same forward and backward.",
+    "github": "https://github.com/Washiy-Codes/Palindrome-Checker",
+    "liveDemo": "https://washiy-codes.github.io/Palindrome-Checker/"
+  },
+  {
+    "title": "Markdown Previewer",
+    "image": "https://shots.codepen.io/username/pen/GrZVVO-800.jpg?version=1623429840",
+    "description": "A simple tool that converts Markdown text into live, formatted HTML in real time.",
+    "github": "https://github.com/Washiy-Codes/Markdown-Previewer",
+    "liveDemo": "https://washiy-codes.github.io/Markdown-Previewer/"
+  },
+  {
+    "title": "Telephone Number Validator",
+    "image": "https://pbs.twimg.com/media/G59UeiQXwAAzLrh.jpg",
+    "description": "A simple tool that validates telephone numbers based on specific patterns.",
+    "github": "https://github.com/Washiy-Codes/Telephone-Number-Validator",
+    "liveDemo": "https://washiy-codes.github.io/Telephone-Number-Validator/"
+  },
+  {
+    "title": "Drum Machine",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWJAeip4BYKMJp6F9SOZyZXGAVoOUncwRhJQ&s",
+    "description": "An interactive web app that plays different drum sound samples when clicked or triggered by keys.",
+    "github": "https://github.com/Washiy-Codes/Drum-Machine",
+    "liveDemo": "https://washiy-codes.github.io/Drum-Machine/"
+  },
+  {
+    "title": "Calculator App",
+    "image": "https://assets.codepen.io/1015609/internal/screenshots/pens/QWvjWbg.default.png?fit=cover&format=auto&ha=true&height=360&quality=75&v=2&version=1625255757&width=640",
+    "description": "A functional web calculator capable of performing standard arithmetic calculations.",
+    "github": "https://github.com/Washiy-Codes/Calculator-App",
+    "liveDemo": "https://washiy-codes.github.io/Calculator-App/"
+  },
+  {
+    "title": "Roman Numeric Converter",
+    "image": "https://global.discourse-cdn.com/freecodecamp/original/4X/9/2/5/925f9fb66ff01653decdaa0c7201b9a58fb917ad.png",
+    "description": "A simple tool that converts Roman numerals to their numeric equivalents and vice versa.",
+    "github": "https://github.com/Washiy-Codes/Roman-Numeric-Converter",
+    "liveDemo": "https://washiy-codes.github.io/ROMAN-NUMERIC-CONVERTER/"
+  },
+  {
+    "title": "Todo App",
+    "image": "https://1.bp.blogspot.com/-i78iKr_P9Pk/X9ohjXyc5eI/AAAAAAAAA_4/8UauxZaOgUshGK7MXwW1gZqts7Zrf_AewCLcBGAsYHQ/s1280/Todo%2BList%2BApp%2Busing%2BHTML%2BCSS%2B%2526%2BJavaScript.webp",
+    "description": "A simple tool that helps users manage their tasks and stay organized.",
+    "github": "https://github.com/Washiy-Codes/Todo-App",
+    "liveDemo": "https://washiy-codes.github.io/Todo-App/"
+  },
+  {
+    "title": "One Time Password Generator (OTP)",
+    "image": "https://global.discourse-cdn.com/freecodecamp/original/4X/2/e/3/2e3577e8b2739fa24975faae30fb2b61c8cb11b6.gif",
+    "description": "Generates an OTP with a countdown timer to indicate when the code expires.",
+    "github": "https://github.com/Washiy-Codes/OTP-Generator?tab=readme-ov-file",
+    "liveDemo": "https://washiy-codes.github.io/OTP-Generator/"
+  }
+]
+
+export { projects}
