160a7eb82e63b94ad0977254dac88df865529e2a
Joseph Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2026-07-24 23:37:18 +0300

    Remove deprecated components (navbar, sidebar, projects)

--- app/components/navbar.tsx
+++
@@ -1,9 +0,0 @@
-export default function Navbar({ isOpen }: { isOpen: boolean }) {
-  return (
-    <div
-      className={`fixed top-0 left-0 w-full bg-teal-500 h-[10%] shadow p-4 transition-transform duration-500 z-40
-      ${isOpen ? "translate-y-0" : "-translate-y-full"}`}
-    >
-    </div>
-  );
-}
