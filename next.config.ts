57c8bd06fe7865f0cdce672048fee5c58300ab04
Joseph Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2026-07-24 23:39:06 +0300

    Update dependencies and Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '://gstatic.com',
+        pathname: '/**',
+      },
+      // 1. Add the missing forum CDN domain here
+      {
+        protocol: 'https',
+        hostname: 'global.discourse-cdn.com',
+        pathname: '/**',
+      },
+    ],
+  },
   /* config options here */
 };
 
