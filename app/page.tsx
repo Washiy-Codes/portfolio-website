b99d01c7b3f89d51f7e4f15651c16d9e7b7c5cc0
Joseph Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2026-07-24 23:38:07 +0300

    Revamp homepage structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,13 @@
-"use client";
-import {useState} from "react";
-import Sidebar from "./components/sidebar";
-
 import Hero from "./components/hero";
-function Homepage() {
-  const [isOpen, setIsOpen] = useState(false);
-  const handleSidebarToggle = () => {
-    setIsOpen(!isOpen);
-  }
 
+function HomePage() {
   return (
-    <div>
-      
-      <Sidebar isOpen={isOpen} />
-      <Hero isOpen={isOpen} onClick={handleSidebarToggle} />
-    </div>
+    <main className="flex min-h-screen flex-col w-[85%] ml-[15%]">
+       <div className="flex items-center justify-center min-h-[calc(100vh-3.5rem)] w-full py-14 px-14 overflow-y-auto overflow-x-hidden bg-[linear-gradient(rgba(0,0,0,0.65),rgba(0,0,0,0.65)),url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQz-QMHbaylpk-NNjiovQFsym6IA1LCxrJeu7Nr9PcAdg&s=10')] bg-cover bg-center">
+         <Hero />
+       </div>
+    </main>
   );
 }
 
-export default Homepage;
+export default HomePage;
