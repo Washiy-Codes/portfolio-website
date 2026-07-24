858ae2246942341b404d6799635b701d6882081a
Joseph Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2026-07-24 23:38:21 +0300

    Update about page content and layout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,9 +2,9 @@
 const About = () => {
 
   return (
-    <div>
-      <h1>About Me</h1>
-      <p>Welcome to my about page!</p>
+    <div className="p-6 bg-gray-950 min-h-[calc(100vh-3.5rem)] w-[85%] ml-[15%] flex flex-col mt-14">
+      <h1 className="text-3xl font-bold mb-4 text-gray-100">About Me</h1>
+      <p className="text-gray-300">Welcome to my about page!</p>
     </div>
   );
 };
