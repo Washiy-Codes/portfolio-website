a6f3152b7cfd9d8a4ad3ddf508a844de8902947d
Joseph Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2026-07-27 22:56:54 +0300

    feat: update portfolio page

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
-import React from 'react'
+import { projects } from '../data'
+
 
 const PortfolioPage = () => {
   return (
@@ -7,12 +8,26 @@ const PortfolioPage = () => {
       <h1 className="text-3xl font-bold mb-4 text-gray-100">Portfolio</h1>
       <p className="text-gray-300 mb-4">Welcome to my portfolio page! Here you can find a collection of my projects and work.</p>
       <p className="text-gray-300">Feel free to explore and check out the details of each project.</p>
-
-      <div className="mt-6">
-        <Link href="/projects" className="text-blue-500 hover:underline flex items-center">
-          <span className="mr-2 flex flex-1 items-center justify-center">View Projects</span>
-        </Link>
-      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {projects.map((project, index) => (
+                    <div key={index} className="bg-white p-4 shadow-md rounded-md flex flex-col justify-between fade-in-from-top">
+                        <div>
+                            <img src={project.image} alt={project.title} width={400} height={300} className="w-full h-48 object-cover mb-4 rounded-sm" />
+                            <h2 className="text-xl font-bold mb-2 text-gray-900">{project.title}</h2>
+                            <p className="text-gray-600 mb-4 text-sm">{project.description}</p>
+                        </div>
+                        
+                        <div className="flex justify-between items-center mt-4">
+                                <Link href={project.github} target="_blank" rel="noopener noreferrer">
+                                    GitHub
+                                </Link>
+                                <Link href={project.liveDemo} target="_blank" rel="noopener noreferrer">
+                                    Live Demo
+                                </Link>
+                        </div>
+                    </div>
+                ))}
+            </div>
     </div>
   )
 }
